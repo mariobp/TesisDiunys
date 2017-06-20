755984a5db2f68fc34b7ea98f1f7a2a4eefe9c22
mariobp <mariobarrpach@gmail.com>
2017-06-20 16:17:35 -0500

    pregunta enunciado a textarea

--- sie/encuesta/models.py
+++ sie/encuesta/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class Pregunta(models.Model):
-    enunciado = models.CharField(max_length=100)
+    enunciado = models.TextField()
     numero = models.IntegerField()
 
     class Meta:
