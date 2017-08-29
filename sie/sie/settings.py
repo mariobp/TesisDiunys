087aa60929936836d25b8f60b4782bb827c5c5b9
mariobp <mariobarrpach@gmail.com>
2017-08-28 21:43:38 -0500

    nuevo correo

--- sie/sie/settings.py
+++ sie/sie/settings.py
@@ -35,8 +35,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mariobarrios@exile.com.co'
-EMAIL_HOST_PASSWORD = 'grwlbpmnvfxlofjz'
+EMAIL_HOST_USER = 'egresadosprosistemas@gmail.com'
+EMAIL_HOST_PASSWORD = 'omyjgsmdtlkolyjg'
 
 INSTALLED_APPS = [
     'custom',
