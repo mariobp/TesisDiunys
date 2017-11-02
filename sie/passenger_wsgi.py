d604dd9b85d35aaec8cce12d15b898988e4f1557
mariobp <mariobarrpach@gmail.com>
2017-11-01 21:16:16 -0500

    passenger

--- sie/passenger_wsgi.py
+++ sie/passenger_wsgi.py
@@ -7,9 +7,12 @@ sys.path.append(cwd)
 INTERP = "/home/ftp_sie/.virtualenvs/diu/bin/python"
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.insert(0, "/home/ftp_sie/.virtualenvs/diu")
-sys.path.insert(13, "/home/ftp_sie/sie.seedprojects.org/TesisDiunys/sie")
+sys.path.insert(0, "/home/ftp_sie/sie.seedprojects.org/TesisDiunys/sie")
+sys.path.insert(0, "/home/ftp_sie/.virtualenvs/diu/bin")
+sys.path.insert(0, "/home/ftp_sie/.virtualenvs/diu/lib/python2.7/site-packages/django")
+sys.path.insert(0, "/home/ftp_sie/.virtualenvs/diu/lib/python2.7/site-packages")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'sie.settings'
+
 from django_fastcgi.servers.fastcgi import runfastcgi
 from django.core.servers.basehttp import get_internal_wsgi_application
 wsgi_application = get_internal_wsgi_application()
