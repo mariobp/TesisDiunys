32d63ede0794d971b6e1f1a9b25385ffa522ef56
mariobp <mariobarrpach@gmail.com>
2017-11-01 21:02:20 -0500

    media

--- sie/sie/settings.py
+++ sie/sie/settings.py
@@ -148,10 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/ftp_sie/sie.seedprojects.org/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = '/home/ftp_sie/sie.seedprojects.org/media/'
 
 
 ADMINS = [('Mario', 'mariobarrpach@gmail.com'), ]
