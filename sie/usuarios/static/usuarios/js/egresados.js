1b87566258b7f72f40adb258414b9d85de904156
Mario Barrios <mariobarrpach@gmail.com>
2018-08-24 00:23:47 -0500

    ajustes en boton importar y exportar

--- sie/usuarios/static/usuarios/js/egresados.js
+++ sie/usuarios/static/usuarios/js/egresados.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $(".right-panel").prepend(`
         <div class="card">
-            <div class="card-content">
-                <a href="/dashboard/usuarios/egresado/import/" class="btn btn-primary import">Importar</a>
-                <a target="_blank" href="/usuarios/export/xls/" class="btn btn-primary import">Exportar</a>
+            <div class="card-content row">
+                <a href="/dashboard/usuarios/egresado/import/" class="btn btn-primary import col s12">Importar</a>
+                <a target="_blank" href="/usuarios/export/xls/" class="btn btn-primary import col s12">Exportar</a>
             </div>
         </div>`);
     $('select').material_select();
